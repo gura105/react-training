051495f3ee7264d32420d45123039b9f8a5f4de1
togo-yamanaka <togo.yamanaka@ariseanalytics.com>
2022-09-25 03:48:39 +0900

    add useref

--- src/App.tsx
+++ src/App.tsx
@@ -1,37 +1,50 @@
-import { FC, useState } from "react";
+import { FC, useRef } from "react";
 import Greet from "./components/Greet";
 // import reactLogo from "./assets/react.svg";
 import "./App.css";
 import Summary from "./components/Summary";
 
-const App: FC = () => (
-  <div className="App">
-    <Greet name="Patty" times={4} />
-    <Summary title="maple town" folded>
-      <p>
-        some message about maple town 1 some message about maple town 1 some
-        message about maple town 1 some message about maple town 1 some message
-        about maple town 1 some message about maple town 1
-      </p>
-      <p>
-        some message about maple town 2 some message about maple town 2 some
-        message about maple town 2 some message about maple town 2 some message
-        about maple town 2 some message about maple town 2
-      </p>
-    </Summary>
+const App: FC = () => {
+  const inputRef = useRef<HTMLInputElement>(null);
 
-    <form>
-      <label htmlFor="favChar">好きなキャラクターは？</label>
-      <select id="favChar" defaultValue="fox">
-        <option value="rabbit">パティ</option>
-        <option value="bear">ボビー</option>
-        <option value="fox">ダイアナ</option>
-        <option value="pig">プリプリン</option>
-      </select>
-    </form>
-    <label htmlFor="favReason">そのキャラクターのどこが好き？</label>
-    <textarea id="favReason" defaultValue="【例】見た目が好き"></textarea>
-  </div>
-);
+  const handleClick = () => {
+    inputRef.current?.focus();
+  };
+
+  return (
+    <div className="App">
+      <Greet name="Patty" times={4} />
+      <Summary title="maple town" folded>
+        <p>
+          some message about maple town 1 some message about maple town 1 some
+          message about maple town 1 some message about maple town 1 some
+          message about maple town 1 some message about maple town 1
+        </p>
+        <p>
+          some message about maple town 2 some message about maple town 2 some
+          message about maple town 2 some message about maple town 2 some
+          message about maple town 2 some message about maple town 2
+        </p>
+      </Summary>
+
+      <form>
+        <label htmlFor="favChar">好きなキャラクターは？</label>
+        <select id="favChar" defaultValue="fox">
+          <option value="rabbit">パティ</option>
+          <option value="bear">ボビー</option>
+          <option value="fox">ダイアナ</option>
+          <option value="pig">プリプリン</option>
+        </select>
+      </form>
+      <label htmlFor="favReason">そのキャラクターのどこが好き？</label>
+      <textarea id="favReason" defaultValue="【例】見た目が好き"></textarea>
+
+      <div>
+        <input type="text" ref={inputRef} />
+        <input type="button" value="フォーカス" onClick={handleClick} />
+      </div>
+    </div>
+  );
+};
 
 export default App;
