7d4c6117807aaec9ec3213e8775f30539c6c5d06
gura105 <shingotanaka9355@gmail.com>
2022-09-25 20:42:17 +0900

    add shohoku 5 list sample

--- src/App.tsx
+++ src/App.tsx
@@ -1,53 +1,50 @@
-import { FC, useRef } from 'react';
-import Greet from 'components/Greet';
+import { FC } from 'react';
+import { Heading } from '@chakra-ui/react';
+import CharacterList from 'components/CharacterList';
+import type { Character } from 'components/CharacterList';
 import 'App.css';
-import Summary from 'components/Summary';
 
-const title = import.meta.env.VITE_APP_TITLE;
 console.dir(import.meta.env);
 
 const App: FC = () => {
-  const inputRef = useRef<HTMLInputElement>(null);
-
-  const handleClick = () => {
-    inputRef.current?.focus();
-  };
+  const characters: Character[] = [
+    {
+      id: 1,
+      name: 'Hanamich Sakuragi',
+      grade: 1,
+      height: 189.2,
+    },
+    {
+      id: 2,
+      name: 'Kaede Rukawa',
+      grade: 1,
+      height: 187,
+    },
+    {
+      id: 3,
+      name: 'Ryota Miyagi',
+      grade: 2,
+      height: 168,
+    },
+    {
+      id: 4,
+      name: 'Micchi',
+      grade: 3,
+    },
+    {
+      id: 5,
+      name: 'Akagi',
+      grade: 3,
+      height: 197,
+    },
+  ];
 
   return (
-    <div className="App">
-      <header className="App-header">
-        <p>{title}</p>
-      </header>
-      <Greet name="Patty" times={4} />
-      <Summary title="maple town" folded>
-        <p>
-          some message about maple town 1 some message about maple town 1 some
-          message about maple town 1 some message about maple town 1 some
-          message about maple town 1 some message about maple town 1
-        </p>
-        <p>
-          some message about maple town 2 some message about maple town 2 some
-          message about maple town 2 some message about maple town 2 some
-          message about maple town 2 some message about maple town 2
-        </p>
-      </Summary>
-
-      <form>
-        <label htmlFor="favChar">好きなキャラクターは？</label>
-        <select id="favChar" defaultValue="fox">
-          <option value="rabbit">パティ</option>
-          <option value="bear">ボビー</option>
-          <option value="fox">ダイアナ</option>
-          <option value="pig">プリプリン</option>
-        </select>
-      </form>
-      <label htmlFor="favReason">そのキャラクターのどこが好き？</label>
-      <textarea id="favReason" defaultValue="【例】見た目が好き"></textarea>
-
-      <div>
-        <input type="text" ref={inputRef} />
-        <input type="button" value="フォーカス" onClick={handleClick} />
-      </div>
+    <div className="container">
+      <Heading size="lg" as="h1" my={12}>
+        SLAM DUNK characters
+      </Heading>
+      <CharacterList school="SHOHOKU HIGH SCHOOL" characters={characters} />
     </div>
   );
 };
